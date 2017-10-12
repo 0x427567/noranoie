c65321cf8b1e0a6bc2da0bd8737cc63c44de11dc
Shawn OY <shawnoy@outlook.com>
2017-10-12 20:12:23 +0800

    Use new method

--- functions/animal.js
+++ functions/animal.js
@@ -3,7 +3,8 @@
 const http = require('http')
 const pug = require('pug')
 const template = pug.compileFile('./views/index.pug')
-const getAnimals = require('../lib/data-gov-tw')
+const OpenData = require('../lib/DataGovTw')
+const opendata = new OpenData
 const response = {
   headers: {
     'Content-Type': 'text/html'
@@ -11,23 +12,22 @@ const response = {
 }
 
 module.exports.get = (event, context, callback) => {
-  console.log(event.queryStringParameters)
-
   const page = (event.queryStringParameters && event.queryStringParameters.page) ? parseInt(event.queryStringParameters.page) : 1
 
-  getAnimals({
+  opendata.query({
+    $top: 24,
     $skip: page
   })
-  .then(animals => {
-    response.statusCode = 200
-    response.body = template({title: '首頁 | 野良の家', animals: animals, prePage: page - 1, nextPage: page + 1, currentPage: page})
+    .then(animals => {
+      response.statusCode = 200
+      response.body = template({title: '首頁 | 野良の家', animals: animals, prePage: page - 1, nextPage: page + 1, currentPage: page})
 
-    callback(null, response)
-  })
-  .catch(err => {
-    response.statusCode = 500
-    response.body = 'Error'
+      callback(null, response)
+    })
+    .catch(err => {
+      response.statusCode = 500
+      response.body = 'Error'
 
-    callback(null, response)
-  })
+      callback(null, response)
+    })
 }
